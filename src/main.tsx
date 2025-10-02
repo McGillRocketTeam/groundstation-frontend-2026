f753f5630fded870a7c9b3f058eb2a5bcac5010e
Léo Mindlin <hello@leomindlin.com>
2025-10-01 22:27:39 -0400

    feat: add font

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import "@/style/fonts.css";
+import "@/style/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { ThemeProvider } from "./components/ui/theme-provider";
-import "./index.css";
 import { router } from "./router/router";
 
 createRoot(document.getElementById("root")!).render(
