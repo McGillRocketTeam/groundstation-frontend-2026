dc66af0c8cb4d94c362edafcd543f8bfbae8341e
Daniel Wei <daniel.wei2006@hotmail.com>
2025-10-25 00:11:11 -0400

    prettier save for previous commit --> updated sidebar with interactive highlighting for card cursors, settings highlighted, add card + sign aligned with dashboards and interactive overflow layout

--- src/components/app/sidebar.tsx
+++ src/components/app/sidebar.tsx
@@ -1,37 +1,40 @@
-import { Link } from "react-router";
 import { dashboardList } from "@/router/router";
-import {AddCardDialog } from "./add-card-dialog";
-import { Button} from "../ui/button";
-
-
+import { Link } from "react-router";
+import { Button } from "../ui/button";
+import { AddCardDialog } from "./add-card-dialog";
 
 export function Sidebar() {
-
-    return (
-        <aside className="h-full w-45 border-r p-3">
-            <div className="flex items-center py-2 mb-2">
-                <span className="mr-2 box-border border-1 py-1 px-2">Dashboards</span>
-                <AddCardDialog trigger={<Button size="sm" variant="outline">+</Button>} />
-            </div>
-            <nav className="flex flex-col ml-2 flex-1 overflow-auto">
-                {dashboardList.map((dashboard) => (
-                    <Link className="hover:bg-[#221809] px-3 py-1"
-                        key = {dashboard.slug}
-                        to = {dashboard.slug}
-                    >
-                        {dashboard.title}
-                    </Link>
-                ))}
-                
-            </nav>
-            <div className="mt-auto py-3 w-full flex justify-center">
-                    <Link
-                        to="/settings"
-                        className="w-full text-left py-1 px-3 hover:bg-[#221809] text-[#FE9A00]"
-                    >
-                        Settings
-                    </Link>
-                </div>
-        </aside>
-    )
+  return (
+    <aside className="h-full w-45 border-r p-3">
+      <div className="mb-2 flex items-center py-2">
+        <span className="mr-2 box-border border-1 px-2 py-1">Dashboards</span>
+        <AddCardDialog
+          trigger={
+            <Button size="sm" variant="outline">
+              +
+            </Button>
+          }
+        />
+      </div>
+      <nav className="ml-2 flex flex-1 flex-col overflow-auto">
+        {dashboardList.map((dashboard) => (
+          <Link
+            className="px-3 py-1 hover:bg-[#221809]"
+            key={dashboard.slug}
+            to={dashboard.slug}
+          >
+            {dashboard.title}
+          </Link>
+        ))}
+      </nav>
+      <div className="mt-auto flex w-full justify-center py-3">
+        <Link
+          to="/settings"
+          className="w-full px-3 py-1 text-left text-[#FE9A00] hover:bg-[#221809]"
+        >
+          Settings
+        </Link>
+      </div>
+    </aside>
+  );
 }
